ed1dda863ca86d0e0e5aed95c6357fdfd413739c
lemonadern <taishi.develop@gmail.com>
2021-09-28 20:35:06 +0900

    feat: global Header #8

---
+++ frontend/components/navigator/Header.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react" 
+import {
+  Stack,
+  Heading,
+  Flex,
+  Text,
+} from "@chakra-ui/react" 
+
+const Header: FC = () => {
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding={4}
+      bg="teal.500"
+      color="white"
+    >
+      <Flex align="center" mr={5}>
+        <Heading as="h1" size="lg" letterSpacing={"tighter"}>
+          ShareTimes
+        </Heading>
+      </Flex>
+    </Flex>
+  ) 
+} 
+
+export default Header 
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,9 +2,12 @@ import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 
+import Header from 'components/navigator/Header'
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Header/>
       <Component {...pageProps} />
     </ChakraProvider>
   )
