c1735e7b71560f0ac8f48b747dfbe01f915e85ba
lemonadern <taishi.develop@gmail.com>
2021-09-29 19:44:26 +0900

    feat: Loading component

---
+++ frontend/components/navigator/LoadingMessage.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react'
+import { HStack, Spinner, Text } from '@chakra-ui/react'
+
+const LoadingMessage: FC = () => {
+    return(
+        <HStack alignContent="baseline">
+            <Spinner
+                thickness="4px"
+                speed="0.7s"
+                emptyColor="gray.200"
+                color="teal"
+                size="lg"
+            />
+            <Text padding="2" margin="auto">
+                読み込み中...
+            </Text>
+        </HStack>
+    )
+}
+
+export default LoadingMessage
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,18 @@
-import {NextPage} from 'next'
+import { NextPage } from 'next'
 
 import { Box, Button, chakra, Spinner } from '@chakra-ui/react'
-import ListControlBar from 'components/pages/list/ListControlBar'
+import LoadingMessage from 'components/navigator/LoadingMessage'
 
 const Home: NextPage = () => {
-  return(
+
+  return (
     <div>
       <h1>Hello World!</h1>
       <Box>
         <chakra.h1 color="tomato">Hello World!</chakra.h1>
       </Box>
+      <LoadingMessage></LoadingMessage>
       <Button colorScheme="blue">Button</Button>
-      <ListControlBar />
     </div>
   )
 }
