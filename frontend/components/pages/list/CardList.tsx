dc3f4707bb442d45abf8289d2e43c1bc520d6fdd
lemonadern <taishi.develop@gmail.com>
2021-10-02 14:21:16 +0900

    feat: list views from api

--- frontend/components/pages/list/CardList.tsx
+++ frontend/components/pages/list/CardList.tsx
@@ -1,20 +1,23 @@
 import { FC } from 'react'
 
 import { Wrap, WrapItem } from '@chakra-ui/react'
-import '@types/WaitingTimeHistories'
+import { WaitingTimeHistoryObject} from 'types/WaitingTimeHistories'
 
 import Card from 'components/pages/list/Card'
 
-const CardList: FC = () => {
+const CardList: FC<WaitingTimeHistoryObject> = (posts) => {
+    console.log( Object.keys(posts) )
     return(
-        <Wrap padding="10" spacing="30px" justify="center">
-        {
-            Array(25).fill(0).map(_ => 
-                <WrapItem>
-                    <Card />
-                </WrapItem>
-            )
-        }
+        <Wrap padding="10" spacing="30px" justify="center" paddingTop="3">
+            {
+                Object.keys( posts ).map(
+                    key => 
+                        <WrapItem key={key}>
+                            <Card key={key} {...posts[key] } />
+                        </WrapItem>
+                    
+                )
+            }
     </Wrap>
     )
 }
