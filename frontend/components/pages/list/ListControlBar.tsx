668897163eaba2bddaaed33d74664f3e979c5667
lemonadern <taishi.develop@gmail.com>
2021-10-02 14:40:29 +0900

    feat: list control bar

--- frontend/components/pages/list/ListControlBar.tsx
+++ frontend/components/pages/list/ListControlBar.tsx
@@ -6,20 +6,23 @@ import { Search2Icon, SmallCloseIcon, ChevronDownIcon } from '@chakra-ui/icons'
 const ListControlBar: FC = () => {
     return(
         <div>
-            <HStack>
-              <InputGroup width="2xl">
+            <HStack >
+              <InputGroup >
                 <InputLeftElement
                     pointerEvents="none"
                     children={<Search2Icon color="gray.300" />}
                 />
-                <Input placeholder="検索する" />
+                <Input id="search" placeholder="検索する" />
                 <InputRightElement 
                     pointerEvents="fill"
-                    children={<IconButton aria-label="Clear Input" icon={<SmallCloseIcon />} />}/>
+                    children={<IconButton aria-label="Clear Input" icon={<SmallCloseIcon />} />}
+                />
                 </InputGroup>
                 <Menu>
-                    <MenuButton as={Button} rightIcon={<ChevronDownIcon marginLeft="6px"/>}>
-                        並べ替える
+                    <MenuButton as={Button} 
+                        rightIcon={<ChevronDownIcon marginLeft="24px"/>}
+                    >
+                        並べ替え
                     </MenuButton>
                     <MenuList>
                         <MenuItem>名前順（前から）</MenuItem>
