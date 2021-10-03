29ed4d2dd03666d2c5681902ba7a02edafcf862c
lemonadern <taishi.develop@gmail.com>
2021-10-03 09:33:36 +0900

    feat: make a11y of form better

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -8,16 +8,16 @@ const Login: NextPage = () => {
             <Text fontSize="25px">ログイン</Text>
             <Box padding="2">
                 <Box paddingBottom="4">
-                    <Text fontSize="large">展示名</Text>
-                    <Select placeholder="選択する">
+                    <label htmlFor="organization"><Text fontSize="large">展示名</Text></label>
+                    <Select id="organization" placeholder="選択する">
                         <option value="option1">2-I</option>
                         <option value="option2">4-I</option>
                         <option value="option3">茶道部</option>
                     </Select>
                 </Box>
                 <Box paddingBottom="4">
-                    <Text fontSize="large">パスワード</Text>
-                    <Input placeholder="パスワードを入力" />
+                    <label htmlFor="password"><Text fontSize="large">パスワード</Text></label>
+                    <Input id="password" placeholder="パスワードを入力" />
                 </Box>
             </Box>
             <Button colorScheme="teal">ログイン</Button>
